db0dde074ecc801b6d32b44afbbcd2278772e761
DanyPM <danielaponce34@aragon.unam.mx>
2024-01-24 12:25:09 -0600

    se agregó información y ejercicios para los ejercicios del DOM y eventos

--- eventos/index.js
+++ eventos/index.js
@@ -7,35 +7,94 @@ window.addEventListener("load", function(){
 })
 
 
-//Me fuí al super y compre para un sandwich
-let compras = ['pan','jamon','lechuga','pan','jitomate',]
-compras.length;
+///------Clickcounter 
+let valor_inicial = 0;
 
-//Que me faltó comprar?
-let falto = compras.push('mayonesa', 'jugo', 'donas')
-console.log(compras)
+    function actualizarResultado() {
+      let observar_resultado = document.getElementById("result2");
+      observar_resultado.innerHTML = `${valor_inicial}`;
+    }
 
+    function clickCounter() {
+      valor_inicial++;
+      actualizarResultado();
+    }
 
-// //Que compre?
-// for( i=0; i < compras.length; i++){
-//   console.log(`Elemento ${i} y la compra fue ${compras[i]}`)
-// }
+    function reiniciarContador() {
+     console.log("Borrar")
+      valor_inicial = 0; 
+      actualizarResultado();
+    }
 
-//me arrependí de las donas que compré -> el metodo pop elimina elementos al final de un arreglo
-compras.splice([6])
+    document.getElementById("incrementar").addEventListener('click', clickCounter);
+    document.getElementById("borrar").addEventListener('click', reiniciarContador);
 
-console.log(compras)
+//----- Conversor 
 
-//Que compre?
- for( i=0; i < compras.length; i++){
-  console.log(`Elemento ${i} y la compra fue ${compras[i]}`)
+function pesos(){
+    let pesos = document.getElementById("pesos").value
+    let euros = 21
+    let resultado = document.getElementById("resultado1")
+
+    resultado.innerHTML = "Euros: "+ (pesos / euros)
 }
 
-//Llegó mi mamá y me trajo otros 5 elementos
+function euros(){
+    let euros = document.getElementById("euros").value
+    let dolar = 1.06
+    let res = document.getElementById("resultado2")
+    
+    res.innerHTML = "Dolares:"+ (euros * dolar)
+}
 
-let comprasmama = ['bolillo','pollo','vegetales','chiles en vinagre', 'salchichas']
-comprasmama.length;
+function dolares(){
+    let dolares = document.getElementById("dolares").value
+    let bitcoin = 0.000060
+    let resul = document.getElementById("resultado3")
 
-//Que cosas tengo en total?
-let total = [...compras, ...comprasmama]
-console.log(total)
+    resul.innerHTML = "Bitcoin:" + (dolares * bitcoin)
+}
+
+//------- ejercicio utilizando ClassName
+
+//.onload() cuando se termine de cargar
+window.onload = function() {
+    // Obtener elementos por su clase
+    let elementosDestacados = document.getElementsByClassName("destacado");
+   console.log(elementosDestacados)
+    // Iterar sobre los elementos y realizar alguna acción
+    //for ([expresiónInicial]; [expresiónCondicional]; [expresiónDeActualización])
+    // el bucle se ejecute mientras el índice i sea menor que la longitud de la colección, asegurándonos de que no intentemos acceder a un índice que esté fuera del rango de la colección.
+    for (let i = 0; i < elementosDestacados.length; i++) {
+      elementosDestacados[i].style.textDecoration = "underline";
+    }
+  };
+
+  //----- Utilización del getElementsByName
+
+  function mostrarGeneroSeleccionado() {
+    // Obtener elementos por su nombre
+    var generoElementos = document.getElementsByName("genero");
+
+    // Iterar sobre los elementos y encontrar el elemento seleccionado
+    for (var i = 0; i < generoElementos.length; i++) {
+      if (generoElementos[i].checked) {
+        // Mostrar el resultado
+        document.getElementById("resultadoGenero").innerText = "Género seleccionado: " + generoElementos[i].value;
+        return; // Salir del bucle una vez que se haya encontrado el elemento seleccionado
+      }
+    }
+
+    // Si no se selecciona ningún género
+    document.getElementById("resultadoGenero").innerText = "Por favor, selecciona un género.";
+  }
+
+  //--------- Onchange
+  document.addEventListener('DOMContentLoaded', function() {
+    var miInput = document.getElementById('miInput');
+    var mensaje = document.getElementById('mensaje');
+
+    miInput.onchange = function() {
+      mensaje.textContent = "Mensaje: Has ingresado tu nombre como " + miInput.value;
+    };
+  });
--- node/script.js
+++ node/script.js
@@ -60,3 +60,37 @@ if (promedio > 70){
 } else {
   console.log(`Reprobado: ${promedio}`)
 }
+
+//-----
+//Me fuí al super y compre para un sandwich
+let compras = ['pan','jamon','lechuga','pan','jitomate',]
+compras.length;
+
+//Que me faltó comprar?
+let falto = compras.push('mayonesa', 'jugo', 'donas')
+console.log(compras)
+
+
+// //Que compre?
+// for( i=0; i < compras.length; i++){
+//   console.log(`Elemento ${i} y la compra fue ${compras[i]}`)
+// }
+
+//me arrependí de las donas que compré -> el metodo pop elimina elementos al final de un arreglo
+compras.splice([6])
+
+console.log(compras)
+
+//Que compre?
+ for( i=0; i < compras.length; i++){
+  console.log(`Elemento ${i} y la compra fue ${compras[i]}`)
+}
+
+//Llegó mi mamá y me trajo otros 5 elementos
+
+let comprasmama = ['bolillo','pollo','vegetales','chiles en vinagre', 'salchichas']
+comprasmama.length;
+
+//Que cosas tengo en total?
+let total = [...compras, ...comprasmama]
+console.log(total)
